e6912a8d3f641745d43835464fa477b42dba8394
Funmi Lawal <82683491+Funmi999@users.noreply.github.com>
2022-06-22 13:13:21 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var title = 'personal Data';
+let myNamesIs = 'Lawal Funmi';
+let myHeight = '5.4 meters';
+let myCountry = 'Nigeria';
+     
+console.log(myNamesIs);
+console.log(myHeight);
+console.log(myCountry);
+
+alert('myNamesIs');
+alert('myHeight');
+alert('myCountry');
+<button onclick="window.print()">Print this page</button>
